5981f3a0e26b13d9645d8eeb0bce4e94112cc7dd
JushBJJ <JushPacis@gmail.com>
2019-01-24 23:06:56 +1000

    Additional memory allocation like malloc,split,merge and free

--- Kernel/include/memory.h
+++ Kernel/include/memory.h
@@ -1,10 +1,18 @@
-#ifndef MEMORY_H
-#define MEMORY_H
-  #include <types.h>
-  
-  void *memcpy(void *dest, void *src,unsigned int n);
-  void *memset(void *dst,int c,unsigned int n);
+#ifndef MEMORY_HEADER
+#define MEMORY_HEADER
 
-  /* MEMCP IS OLD AND ONLY USED FOR screen.c*/
-  void memcp(u8 *source,u8 *destination,int bytes);
+#include <types.h>
+
+/* Functions */
+
+void *memcpy(void *dest, void *src,unsigned int n);
+void *memset(void *dst,int c,unsigned int n);
+void *malloc(unsigned int size);
+void initialize();
+void split(struct Malloc_Block *Fitting_Slot_Size,unsigned int size);
+void merge();
+void free(void *ptr);
+
+/* MEMCP IS OLD AND ONLY USED FOR screen.c */
+void memcp(u8 *source,u8 *destination,int bytes);
 #endif
