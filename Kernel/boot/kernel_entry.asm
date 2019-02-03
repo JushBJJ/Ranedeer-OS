6f2cce8d1ed0127e1b0943248e373ad4cc911997
JushBJJ <JushPacis@gmail.com>
2019-02-03 18:03:42 +1000

    Libc

--- Kernel/boot/kernel_entry.asm
+++
@@ -1,9 +0,0 @@
-[bits 32]
-[extern startkernel]
-
-global _start
-
-_start:
- ; Define calling point. Must have same name as kernel.c 'main' function
-call startkernel ; Calls the C function. The linker will know where it is placed in memory
-jmp $
