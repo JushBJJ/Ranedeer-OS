9e9668dd3b3c807dc1d7f0165c57e3b4b013e84e
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2019-04-12 21:12:31 +1000

    Updated Keyboard method and beautified

--- Kernel/libc/Drivers/keyboard.c
+++ Kernel/libc/Drivers/keyboard.c
@@ -7,278 +7,350 @@
 
 void KeyboardOut(u8 scancode);
 
-void EnableKeyboard(){
-  EnableInterrupts();
+void EnableKeyboard()
+{
+    EnableInterrupts();
 }
 
-void DisableKeyboard(){
-  DisableInterrupts();
+void DisableKeyboard()
+{
+    DisableInterrupts();
 }
 
-void SetCursorBarrier(int offset){
-  IN->BARRIER=offset;
+void SetCursorBarrier(int offset)
+{
+    IN->BARRIER = offset;
 }
 
-void SetCursorBarrierHere(){
-  IN->BARRIER=GetCursorOffset();
+void SetCursorBarrierHere()
+{
+    IN->BARRIER = GetCursorOffset();
 }
 
-void DisableCursorBarrier(){
-  IN->BARRIER=-1;
+void DisableCursorBarrier()
+{
+    IN->BARRIER = -1;
 }
 
-void DisableInput(){
-  InputEnabled=0;
+void DisableInput()
+{
+    InputEnabled = 0;
 }
 
-void EnableInput(){
-  InputEnabled=1;
+void EnableInput()
+{
+    InputEnabled = 1;
 }
 
-static void KeyboardCallback(registersT regs){
-  u8 scancode=PBI(0x60);
-  KeyboardOut(scancode);
-  unused(regs);
+static void KeyboardCallback(registersT regs)
+{
+    u8 scancode = PBI(0x60);
+    KeyboardOut(scancode);
+    unused(regs);
 }
 
-void initKeyboard(){
-  RegisterInterruptHandler(IRQ1,KeyboardCallback);
+void initKeyboard()
+{
+    RegisterInterruptHandler(IRQ1, KeyboardCallback);
 }
-void KeyboardOut(u8 scancode){
-  switch(scancode){
-  case 0x0:
-    printo("ERROR");
-  break;
-  case 0x1:
-    /*esc*/
-  break;
-  case 0x2:
-    printo("1");
-    AddtoChar('1');
-  break;
-  case 0x3:
-    printo("2");
-    AddtoChar('2');
-  break;
-  case 0x4:
-    printo("3");
-    AddtoChar('3');
-  break;
-  case 0x5:
-    printo("4");
-    AddtoChar('4');
-  break;
-  case 0x6:
-    printo("5");
-    AddtoChar('5');
-  break;
-  case 0x7:
-    printo("6");
-    AddtoChar('6');
-  break;
-  case 0x8:
-    printo("7");
-    AddtoChar('7');
-  break;
-  case 0x9:
-    printo("8");
-    AddtoChar('8');
-  break;
-  case 0x0A:
-    printo("9");
-    AddtoChar('9');
-  break;
-  case 0x0B:
-    printo("0");
-    AddtoChar('0');
-  break;
-  case 0x0C:
-    printo("-");
-    AddtoChar('-');
-  break;
-  case 0x0D:
-    printo("+");
-    AddtoChar('+');
-  break;
-  case 0x0E:
-    if(GetCursorOffset()!=IN->BARRIER){
-      Backspace();
-    }
-    BackspaceChar();
-  break;
-  case 0x0F:
-    printo("    ");
-    AddtoChar(' ');
-    AddtoChar(' ');
-    AddtoChar(' ');
-    AddtoChar(' ');
-  break;
-  case 0x10:
-    printo("Q");
-    AddtoChar('Q');
-  break;
-  case 0x11:
-    printo("W");
-    AddtoChar('W');
-  break;
-  case 0x12:
-    printo("E");
-    AddtoChar('E');
-  break;
-  case 0x13:
-    printo("R");
-    AddtoChar('R');
-  break;
-  case 0x14:
-    printo("T");
-    AddtoChar('T');
-  break;
-  case 0x15:
-    printo("Y");
-    AddtoChar('Y');
-  break;
-  case 0x16:
-    printo("U");
-    AddtoChar('U');
-  break;
-  case 0x17:
-    printo("I");
-    AddtoChar('I');
-  break;
-  case 0x18:
-    printo("O");
-    AddtoChar('O');
-  break;
-  case 0x19:
-    printo("P");
-    AddtoChar('P');
-  break;
-  case 0x1A:
-    printo("[");
-    AddtoChar('[');
-  break;
-  case 0x1B:
-    printo("]");
-    AddtoChar(']');
-  break;
-  case 0x1C:
-    printo("\n");
-    AddtoChar('\n');
-    break;
-  case 0x1D:
-    /*Left Ctrl*/
-  break;
-  case 0x1E:
-    printo("A");
-    AddtoChar('A');
-  break;
-  case 0x1F:
-    printo("S");
-    AddtoChar('S');
-  break;
-  case 0x20:
-    printo("D");
-    AddtoChar('D');
-  break;
-  case 0x21:
-    printo("F");
-    AddtoChar('F');
-  break;
-  case 0x22:
-    printo("G");
-    AddtoChar('G');
-  break;
-  case 0x23:
-    printo("H");
-    AddtoChar('H');
-  break;
-  case 0x24:
-    printo("J");
-    AddtoChar('J');
-  break;
-  case 0x25:
-    printo("K");
-    AddtoChar('K');
-  break;
-  case 0x26:
-    printo("L");
-    AddtoChar('L');
-  break;
-  case 0x27:
-    printo(";");
-    AddtoChar(';');
-  break;
-  case 0x28:
-    printo("'");
-    AddtoChar('\'');
-  break;
-  case 0x29:
-    printo("`");
-    AddtoChar('\'');
-  break;
-  case 0x2A:
-    /*Left Shift*/
-  break;
+
+void KeyboardOut(u8 scancode)
+{
+    switch (scancode)
+    {
+    // Unknown
+    case 0x0:
+        printo("ERROR");
+        asm("hlt");
+        break;
+
+    // Escape Key (esc)
+    case 0x1:
+        /*esc*/
+        break;
+
+    // 1
+    case 0x2:
+        Key[0] = 0x0000D1;
+        break;
+
+    // 2
+    case 0x3:
+        Key[0] = 0x0000D2;
+        break;
+
+    // 3
+    case 0x4:
+        Key[0] = 0x0000D3;
+        break;
+
+    // 4
+    case 0x5:
+        Key[0] = 0x0000D4;
+        break;
+
+        // 5
+    case 0x6:
+        Key[0] = 0x0000D5;
+        break;
+
+    // 6
+    case 0x7:
+        Key[0] = 0x0000D6;
+        break;
+
+    // 7
+    case 0x8:
+        Key[0] = 0x0000D7;
+        break;
+
+    // 8
+    case 0x9:
+        Key[0] = 0x0000D8;
+        break;
+
+    // 9
+    case 0x0A:
+        Key[0] = 0x0000D9;
+        break;
+
+    // 0
+    case 0x0B:
+        Key[0] = 0x0000DA;
+        break;
+
+    // -
+    case 0x0C:
+        Key[0] = 0x0000DB;
+        break;
+
+    // +
+    case 0x0D:
+        Key[0] = 0x0000DC;
+        break;
+
+    // backspace
+    case 0x0E:
+        Key[0] = 0x0000DD;
+        break;
+
+    // Tab
+    case 0x0F:
+        Key[0] = 0x0000DE;
+        break;
+
+    // Q
+    case 0x10:
+        Key[0] = 0x0000DF;
+        break;
+
+    // W
+    case 0x11:
+        Key[0] = 0x0000DF1;
+        break;
+
+    // E
+    case 0x12:
+        Key[0] = 0x0000DF2;
+        break;
+
+    // R
+    case 0x13:
+        Key[0] = 0x0000DF3;
+        break;
+
+    // T
+    case 0x14:
+        Key[0] = 0x0000DF4;
+        break;
+
+    // Y
+    case 0x15:
+        Key[0] = 0x0000DF5;
+        break;
+
+    // U
+    case 0x16:
+        Key[0] = 0x0000DF6;
+        break;
+
+    // I
+    case 0x17:
+        Key[0] = 0x0000DF7;
+        break;
+
+    // O
+    case 0x18:
+        Key[0] = 0x0000DF8;
+        break;
+
+    // P
+    case 0x19:
+        Key[0] = 0x0000DF9;
+        break;
+
+    // [
+    case 0x1A:
+        Key[0] = 0x0000DFA;
+        break;
+
+    // ]
+    case 0x1B:
+        Key[0] = 0x0000DFB;
+        break;
+
+    // Enter
+    case 0x1C:
+        Key[0] = 0x0000DFC;
+        break;
+
+    // Left Ctrl
+    case 0x1D:
+        Key[0] = 0x0000DFD;
+        break;
+
+    // A
+    case 0x1E:
+        Key[0] = 0x0000DFE;
+        break;
+
+    // S
+    case 0x1F:
+        Key[0] = 0x0000DFF;
+        break;
+
+    // D
+    case 0x20:
+        Key[0] = 0x0000DE1;
+        break;
+
+    // F
+    case 0x21:
+        Key[0] = 0x0000DE2;
+        break;
+
+    // G
+    case 0x22:
+        Key[0] = 0x0000DE3;
+        break;
+
+    // H
+    case 0x23:
+        Key[0] = 0x0000DE4;
+        break;
+
+    // J
+    case 0x24:
+        Key[0] = 0x0000DE5;
+        break;
+
+    // K
+    case 0x25:
+        Key[0] = 0x0000DE5;
+        break;
+
+    // L
+    case 0x26:
+        Key[0] = 0x0000DE6;
+        break;
+
+    // ;
+    case 0x27:
+        Key[0] = 0x0000DE7;
+        break;
+
+    // '
+    case 0x28:
+        Key[0] = 0x0000DE8;
+        break;
+
+    // `
+    case 0x29:
+        Key[0] = 0x0000DE9;
+        break;
+
+    // Left Shift
+    case 0x2A:
+        Key[0] = 0x0000DEA;
+        break;
+
+        // \ 
   case 0x2B:
-    printo("\\");
-    AddtoChar('\\');
-  break;
-  case 0x2C:
-    printo("Z");
-    AddtoChar('Z');
-  break;
-  case 0x2D:
-    printo("X");
-    AddtoChar('X');
-  break;
-  case 0x2E:
-    printo("C");
-    AddtoChar('C');
-  break;
-  case 0x2F:
-    printo("V");
-    AddtoChar('V');
-  break;
-  case 0x30:
-    printo("B");
-    AddtoChar('B');
-  break;
-  case 0x31:
-    printo("N");
-    AddtoChar('N');
-  break;
-  case 0x32:
-    printo("M");
-    AddtoChar('M');
-  break;
-  case 0x33:
-    printo(",");
-    AddtoChar(',');
-  break;
-  case 0x34:
-    printo(".");
-    AddtoChar('.');
-  break;
-  case 0x35:
-    printo("/");
-    AddtoChar('/');
-  break;
-  case 0x36:
-    /*Right shift*/
-  break;
-  case 0x37:
-    printo("*");
-    AddtoChar('*');
-  break;
-  case 0x38:
-    /*left alt*/
-  break;
-  case 0x39:
-    printo(" ");
-    AddtoChar(' ');
-  break;
+        Key[0] = 0x0000DEB;
+        break;
 
-  default:
-    break;
-}
+    // Z
+    case 0x2C:
+        Key[0] = 0x0000DEC;
+        break;
+
+    // X
+    case 0x2D:
+        Key[0] = 0x0000DED;
+        break;
+
+    // C
+    case 0x2E:
+        Key[0] = 0x0000DEE;
+        break;
+
+    //V
+    case 0x2F:
+        Key[0] = 0x0000DEF;
+        break;
+
+    // B
+    case 0x30:
+        Key[0] = 0x0000DD1;
+        break;
+
+    // N
+    case 0x31:
+        Key[0] = 0x0000DD2;
+        break;
+
+    //M
+    case 0x32:
+        Key[0] = 0x0000DD3;
+        break;
+
+    // ,
+    case 0x33:
+        Key[0] = 0x0000DD4;
+        break;
+
+    // .
+    case 0x34:
+        Key[0] = 0x0000DD5;
+        break;
+
+    // /
+    case 0x35:
+        Key[0] = 0x0000DD6;
+        break;
+
+    // Right Shift
+    case 0x36:
+        Key[0] = 0x0000DD7;
+        break;
+
+    // *
+    case 0x37:
+        Key[0] = 0x0000DD8;
+        break;
+
+    // Left Alt
+    case 0x38:
+        Key[0] = 0x0000DD9;
+        break;
+
+    // Space
+    case 0x39:
+        Key[0] = 0x0000DDA;
+        break;
+
+    // Unknown
+    default:
+        break;
+    }
 }
