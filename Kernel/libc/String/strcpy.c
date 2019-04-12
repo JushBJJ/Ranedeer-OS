318985cf804ce1cfe1d522cfe29ae2428ce3539f
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2019-04-12 21:27:35 +1000

    Update strcpy.c

--- Kernel/libc/String/strcpy.c
+++ Kernel/libc/String/strcpy.c
@@ -1,7 +1,6 @@
 #include <string.h>
 #include <memory.h>
 
-/* From `man strcpy` */
 char *strncpy(char *Origin, char *Copied, size_t SizeOfString){  
   for(size_t i=0; i<SizeOfString+1; i++){
     Origin[i]=Copied[i];
@@ -15,3 +14,25 @@ char *strcpy(char *Origin,char *Copied){
   dest = strncpy(Origin, Copied, strlen(Copied);
   return dest;
 }
+                 
+/* m_strcpy (Used when memory was already alocated) */
+void m_strcpy(char *x, char *y)
+{
+    strcpy(x, y); // Copy the shit first
+    for (int i = 0;; i++)
+    {
+        if (x[i] == 0x13)
+        {
+            i--;
+            x[i] = 0x01;
+            break;
+        }
+        if (x[i] == 0x00)
+        {
+            x[i] = 0x01;
+        }
+    }
+    // Intelletual 100, hardest code ever written
+    return;
+}
+
