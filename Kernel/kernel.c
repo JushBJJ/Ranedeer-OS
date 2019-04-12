3945bf96682432f10eb37fb862d7e582ca465e01
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2019-04-12 21:22:03 +1000

    Update kernel.c

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -5,30 +5,31 @@
 #include <color.h>
 #include <timer.h>
 #include <Shell.h>
+#include <keyboard.h>
 
 void startkernel(){
-  	clear();
+  	clear(); // Clear Screen
+	
   	printo("%kKernel Loading\n",White_Text);
+	
   	/* Interrupt Initialization */
-  	I_CK->Keyboard=false;
+  	I_CK->Keyboard=false; // Disable the keyboard
   	I_CK->Call=true; /* ON BY DEFAULT */
-	isr_install();
+	isr_install(); //Install interrupts
 
 	initTimer(50);
 	initKeyboard();
-
-	EnableInterrupts();
-	if(I_CK->Call==false)
-		I_CK->Call=true;
-	if(I_CK->Keyboard==true)
-		I_CK->Keyboard=false;
-
-	DisableKeyboard();
+	
+	/* Just to make sure that all of the Key's arrays are 0x00000 */
+	for(int i=0;i<sizeof(Key);i++){
+		Key[i]=0x00;
+	}
+	
+	EnableInterrupts(); // Enable Interrupts
+	DisableKeyboard(); // Disable keyboard??? lol wat
 		
   	printk("Kernel: Interrupts [%kSyscall%k] %k%b%k\n",Light_Green_Text,White_Text,Light_Green_Text,I_CK->Call,White_Text);
 	printk("Kernel: Interrupts [%kKeyboard%k] %k%b%k\n",Light_Green_Text,White_Text,Light_Red_Text,I_CK->Keyboard,White_Text);
-	Start_Shell();
-
-	int x;
-	while(x=0){}
+	
+	Start_Shell(); // Start the shell..NOW
 }
