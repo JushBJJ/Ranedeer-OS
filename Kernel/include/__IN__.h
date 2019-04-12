e1e5717a295e685508e17e97ea2d393682c7b5cc
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2019-04-12 21:36:36 +1000

    Update __IN__.h

--- Kernel/include/__IN__.h
+++ Kernel/include/__IN__.h
@@ -2,13 +2,7 @@
   #define __IN__H
   #include <bool.h>
 
-  struct INPUT{
-    char __IN__[1024]; /* 1 kb */
-    bool __INPUT_DONE;
-    int __pointer;
-    int BARRIER;
-    struct INPUT *__NEXT;
-  };
+  char INPUT_BLOCK[1024]; // 1 KB
 
   void SetCursorBarrier(int offset);
   void SetCursorBarrierHere();
